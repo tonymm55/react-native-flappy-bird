ee3dfc172ad58bfc52e678c25ae5bdef4e014752
TonyMM55 <tonymmoran@gmail.com>
2023-05-01 21:37:19 +0100

    moving obstacles

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component }from 'react';
+import { Dimensions, StyleSheet, Text, View, StatusBar, Alert, TouchableOpacity, Image } from "react-native";
+import Constants from "./Constants/Constants";
+import { GameEngine } from "react-native-game-engine";
+import Matter from "matter-js";
+import Bird from "./Components/Bird"
+import Wall from "./Components/Wall"
+import Physics from "./Components/Physics";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export const randomBetween = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+export const generatePipes = () => {
+  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
+  let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
+
+  let sizes = [topPipeHeight, bottomPipeHeight];
+
+  if (Math.random() < 0.5) {
+    sizes = sizes.reverse();
+  }
+
+  return sizes;
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.gameEngine = null;
+    this.entities = this.setupWorld();
+
+    this.state = {
+      running: true,
+    }
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false });
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
+    let floor = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT -25, Constants.MAX_WIDTH, 50, { isStatic: true });
+    let ceiling = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, 25, Constants.MAX_WIDTH, 50, { isStatic: true });
+
+    let [pipe1Height, pipe2Height] = generatePipes();
+    let pipe1 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), pipe1Height / 2, Constants.PIPE_WIDTH, pipe1Height, { isStatic: true });
+    let pipe2 = Matter.Bodies.rectangle( Constants.MAX_WIDTH - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe2Height / 2), Constants.PIPE_WIDTH, pipe2Height, { isStatic: true });
+
+    let [pipe3Height, pipe4Height] = generatePipes();
+    let pipe3 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), pipe3Height / 2, Constants.PIPE_WIDTH, pipe3Height, { isStatic: true });
+    let pipe4 = Matter.Bodies.rectangle( Constants.MAX_WIDTH * 2 - (Constants.PIPE_WIDTH / 2), Constants.MAX_HEIGHT - (pipe4Height / 2), Constants.PIPE_WIDTH, pipe4Height, { isStatic: true });
+
+    Matter.World.add(world, [bird, floor, ceiling, pipe1, pipe2, pipe3, pipe4]);
+
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, size: [50, 50], color: "red", renderer: Bird },
+      floor: { body: floor, size: [Constants.MAX_WIDTH, 150], color: "green", renderer: Wall },
+      ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 150], color: "aqua", renderer: Wall },
+      pipe1: { body: pipe1, size: [Constants.PIPE_WIDTH, pipe1Height], color: "purple", renderer: Wall },
+      pipe2: { body: pipe2, size: [Constants.PIPE_WIDTH, pipe2Height], color: "red", renderer: Wall },
+      pipe3: { body: pipe3, size: [Constants.PIPE_WIDTH, pipe3Height], color: "blue", renderer: Wall },
+      pipe4: { body: pipe4, size: [Constants.PIPE_WIDTH, pipe4Height], color: "yellow", renderer: Wall },
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GameEngine
+          ref={(ref) => { this.gameEngine = ref; }}
+          style={styles.gameContainer}
+          systems={[Physics]}
+          // running={this.state.running}
+          entities={this.entities} />
+          <StatusBar hidden={true} />
+      </View>
+    )
+  }  
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  gameContainer: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0, 
   },
 });
