4e60581b502e5af10aa1b0ff4360b92a20cc7504
TonyMM55 <tonymmoran@gmail.com>
2023-05-03 18:03:44 +0200

    addtion of pipes and changes to Physics

--- App.js
+++ App.js
@@ -3,29 +3,13 @@ import { Dimensions, StyleSheet, Text, View, StatusBar, Alert, TouchableOpacity,
 import Matter from "matter-js";
 import { GameEngine } from "react-native-game-engine";
 import Bird from "./Components/Bird";
-import Wall from "./Components/Wall";
 import Floor from "./Components/Floor";
 import Physics from "./Components/Physics";
 import Constants from "./Constants/Constants";
 import Images from "./assets/Images";
 
 
-export const randomBetween = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
-export const generatePipes = () => {
-  let topPipeHeight = randomBetween(100, (Constants.MAX_HEIGHT / 2) - 100);
-  let bottomPipeHeight = Constants.MAX_HEIGHT - topPipeHeight - Constants.GAP_SIZE;
-
-  let sizes = [topPipeHeight, bottomPipeHeight];
-
-  if (Math.random() < 0.5) {
-    sizes = sizes.reverse();
-  }
-
-  return sizes;
-}
 
 export default class App extends Component {
   constructor(props) {
@@ -43,20 +27,21 @@ export default class App extends Component {
     let world = engine.world;
     engine.gravity.y = 0.0;
 
-    let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT / 2, 50, 50);
-    let floor1 = Matter.Bodies.rectangle( 
-      Constants.MAX_WIDTH / 2, 
-      Constants.MAX_HEIGHT - 25, 
-      Constants.MAX_WIDTH, + 4, 50, 
+    let bird = Matter.Bodies.rectangle( Constants.MAX_WIDTH / 2, Constants.MAX_HEIGHT / 2, Constants.BIRD_WIDTH, Constants.BIRD_HEIGHT);
+    
+    let floor1 = Matter.Bodies.rectangle(
+      Constants.MAX_WIDTH / 2,
+      Constants.MAX_HEIGHT - 25,
+      Constants.MAX_WIDTH + 4, 50,
       { isStatic: true }
     );
 
-    let floor2 = Matter.Bodies.rectangle( 
-      Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2), 
-      Constants.MAX_HEIGHT - 25, 
-      Constants.MAX_WIDTH, + 4, 50, 
-      { isStatic: true }
-      );
+    let floor2 = Matter.Bodies.rectangle(
+        Constants.MAX_WIDTH + (Constants.MAX_WIDTH / 2),
+        Constants.MAX_HEIGHT - 25,
+        Constants.MAX_WIDTH + 4, 50,
+        { isStatic: true }
+    );
 
     Matter.World.add(world, [bird, floor1, floor2]);
 
@@ -70,7 +55,7 @@ export default class App extends Component {
       physics: { engine: engine, world: world },
       floor1: { body: floor1, renderer: Floor },
       floor2: { body: floor2, renderer: Floor },
-      bird: { body: bird, size: [50, 50], color: "red", renderer: Bird },
+      bird: { body: bird, pose: 1, renderer: Bird },
     }
   }
 
--- Components/PipeTop.js
+++ Components/PipeTop.js
@@ -2,40 +2,24 @@ import React, { Component } from "react";
 import { View, Image, Animated } from "react-native";
 import Images from "../assets/Images";
 
-export default class Bird extends Component {
-  constructor(props) {
-    super(props);
-
-    this.animatedValue = new Animated.Value(this.props.body.velocity.y);
-  }
-
+export default class PipeTop extends Component {
     render() {
       const width = this.props.body.bounds.max.x - this.props.body.bounds.min.x;
       const height = this.props.body.bounds.max.y - this.props.body.bounds.min.y;
         const x = this.props.body.position.x - width / 2;
         const y = this.props.body.position.y - height / 2;
 
-        this.animatedValue.setValue(this.props.body.velocity.y);
-        let rotation = this.animatedValue.interpolate({
-          inputRange: [-10, 0, 10, 20],
-          outputRange: ["-15deg", "0deg", "15deg", "45deg"],
-          extrapolate: "clamp"
-        })
-
-        let image = Images["bird" + this.props.pose];
-
         return (
-            <Animated.Image
+            <Image
               style={{
                 position: "absolute",
                 left: x,
                 top: y,
                 width: width,
                 height: height,
-                transform: [{ rotate: rotation }]
               }} 
               resizeMode="stretch"
-              source={image} />
+              source={Images.pipeTop} />
         );
     }
 }
